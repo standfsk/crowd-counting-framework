4ee12d38bcaf2b008029bbc3e23371c24513b23f
standfsk <60250322+standfsk@users.noreply.github.com>
2025-09-24 14:19:35 +0900

    Update prepare.py
    changed value "val" -> "valid"

--- datasets/prepare.py
+++ datasets/prepare.py
@@ -3,10 +3,10 @@ import os
 
 def mktxt() -> None:
     train_image_paths = sorted(glob.glob(os.path.join("**", "train", "*.jpg")))
-    val_image_paths = sorted(glob.glob(os.path.join("**", "val", "*.jpg")))
+    val_image_paths = sorted(glob.glob(os.path.join("**", "valid", "*.jpg")))
     test_image_paths = sorted(glob.glob(os.path.join("**", "test", "*.jpg")))
 
-    for subset, image_paths in [["train", train_image_paths], ["val", val_image_paths], ["test", test_image_paths]]:
+    for subset, image_paths in [["train", train_image_paths], ["valid", valid_image_paths], ["test", test_image_paths]]:
             with open(f"{subset}.txt", "w") as txt_file:
                 for image_path in image_paths:
                     txt_file.write(f"{os.path.abspath(image_path)}\n")
